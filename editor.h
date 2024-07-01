7847afe940b225702be9dc62de8887932f9af992
drclcomputers <132164125+drclcomputers@users.noreply.github.com>
2024-07-01 11:35:17 +0300

    Update editor.h

--- editor.h
+++ editor.h
@@ -1,3 +1,10 @@
+#include<iostream>
+#include<fstream>
+#include<stdlib.h>
+#include<string.h>
+
+using namespace std;
+
 int main()
 {
     char action; string text, data, filename, data2[10000]; int aux=0;
