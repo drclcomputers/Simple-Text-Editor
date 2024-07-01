90a75a29554643925183e6949dbe82f8b5ef2f75
drclcomputers <132164125+drclcomputers@users.noreply.github.com>
2024-07-01 11:38:09 +0300

    Update editor.hpp

--- editor.hpp
+++ editor.hpp
@@ -7,16 +7,25 @@ using namespace std;
 
 int main()
 {
-    char action; string text, data, filename, data2[10000]; int aux=0;
+    char action; string text, data, filename, data2[10000]; 
+    int aux=0;
+    
     while(action!='e'){
-        data="", filename.clear(), text.clear(); cout<<"Simple Text Editor: C++ Edition"<<endl<<"Write (w), Open (o), Exit (e): "; cin>>action;
+        data="", filename.clear(), text.clear(); 
+        
+        cout<<"Simple Text Editor: C++ Edition"<<endl<<"Write (w), Open (o), Exit (e): "; cin>>action;
 
         if(action=='w'){
             while(text!="exit"){
                 getline(cin, text);
                 if(text!="exit") data+=text+"\n"; }
             cout<<"Input Filename: "; getline(cin, filename);
-            if(filename!=""){ ofstream fout; fout.open(filename); fout<<data; fout.close(); }
+            if(filename!=""){ 
+                ofstream fout; 
+                fout.open(filename); 
+                fout<<data; 
+                fout.close(); 
+            }
         }
 
         if(action=='o'){
@@ -30,6 +39,7 @@ int main()
                 while(getline(fin, data)){
                     data2[aux]=data;
                     aux++;}
+                
                 while(line!=0){
                     for(int i=0; i<aux; i++)
                         cout<<i+1<<" "<<data2[i]<<endl;
@@ -44,12 +54,17 @@ int main()
                         fin.clear(); fin.seekg(0);
                         for(int i=0; i<aux; i++)
                             fin<<data2[i]<<endl;}
-                    system("cls");}
+                    
+                    system("cls");
+                }
+                
                 for(int i=0; i<aux; i++)
                     data2[i]="";
                 aux=0;
                 fin.close();
-            }else{
+            }
+            else
+            {
                 cout<<"File appears to not exist"<<endl<<"Try again (a) or Exit (e): ";
                 string a; cin>>a;
                 if(a!="a")
@@ -57,5 +72,7 @@ int main()
         }
         system("cls");
     }
+    
     return 0;
+    
 }
